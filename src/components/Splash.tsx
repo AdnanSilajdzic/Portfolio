7989a093b192e1e476caebe9c59f69adbfc59dea
Adnan <adnan.silajdzic.it@gmail.com>
2023-10-19 19:58:52 +0200

    refactored binary text function

--- src/components/Splash.tsx
+++ src/components/Splash.tsx
@@ -1,52 +1,23 @@
 import { useState, useEffect } from 'react';
 import BinaryTextEffect from '../functions/BinaryTextEffect';
-import TypingTextEffect from '../functions/TypingTextEffect';
 
 const Splash = () => {
 	const [trueText, setTrueText] = useState('Hi, my name is Adnan');
-	const [text, setText] = useState('');
-
+	const [text, setText] = useState('000 00 1011 11 01011');
 	const [trueSubText, setTrueSubText] = useState(
 		'and I am a software engineer'
 	);
-	const [subText, setSubText] = useState('');
-
-	const typingTop = () => {
-		let i = -1;
-		const intervalId = setInterval(async () => {
-			i++;
-			if (i <= trueText.length) {
-				setText(BinaryTextEffect(trueText, i));
-			} else {
-				clearInterval(intervalId); // Stop the interval when all elements are processed
-			}
-		}, 60);
-	};
-	const typingBottom = () => {
-		let j = -1;
-		const intervalId = setInterval(async () => {
-			j++;
-			if (j <= trueSubText.length) {
-				setSubText(BinaryTextEffect(trueSubText, j));
-			} else {
-				clearInterval(intervalId); // Stop the interval when all elements are processed
-			}
-		}, 60);
-	};
+	const [subText, setSubText] = useState('111 0 01 0 111010111 01001110');
 
 	useEffect(() => {
-		setSubText(BinaryTextEffect(trueSubText, 0));
-		setText(BinaryTextEffect(trueText, 0));
 		setTimeout(() => {
-			typingTop();
-			setTimeout(typingBottom, 1400);
+			BinaryTextEffect(trueText, setText);
+			setTimeout(() => BinaryTextEffect(trueSubText, setSubText), 1400);
 		}, 800);
 	}, []);
 
 	return (
-		<div
-			className={`flex h-screen flex-col items-center justify-center text-center font-almamono text-4xl text-white sm:text-7xl`}
-		>
+		<div className="flex h-screen flex-col items-center justify-center text-center font-almamono text-4xl text-white sm:text-7xl">
 			<div className="splash z-30 flex flex-col items-center justify-center gap-6">
 				<h1>{text}</h1>
 				<h2 className="text-lg sm:text-3xl">{subText}</h2>
--- src/functions/BinaryTextEffect.ts
+++ src/functions/BinaryTextEffect.ts
@@ -1,14 +1,25 @@
-export default function BinaryTextEffect(text: string, index: number) {
-	const binaryTextStart = text.slice(0, index);
-	const binaryTextEnd = text
-		.slice(index)
-		.split('')
-		.map((char) => {
-			if (char === ' ') return ' ';
-			const number = Math.floor(Math.random() * 2);
-			return number === 0 ? '0' : '1';
-		})
-		.join('');
-	const binaryText = binaryTextStart + binaryTextEnd;
-	return binaryText;
+export default function BinaryTextEffect(
+	text: string,
+	setState: React.Dispatch<React.SetStateAction<string>>
+) {
+	let i = -1;
+	const intervalId = setInterval(async () => {
+		i++;
+		if (i <= text.length) {
+			const binaryTextStart = text.slice(0, i);
+			const binaryTextEnd = text
+				.slice(i)
+				.split('')
+				.map((char) => {
+					if (char === ' ') return ' ';
+					const number = Math.floor(Math.random() * 2);
+					return number === 0 ? '0' : '1';
+				})
+				.join('');
+			const binaryText = binaryTextStart + binaryTextEnd;
+			setState(binaryText);
+		} else {
+			clearInterval(intervalId); // Stop the interval when all elements are processed
+		}
+	}, 65);
 }
